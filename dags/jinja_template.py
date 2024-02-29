c98ed4f27fb14af05ab2d2180a0c6da348d31163
nitsvutt <nitsvutt@gmail.com>
2024-02-29 23:28:20 +0700

    jinja template

---
+++ dags/jinja_template.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'vutt',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 1, 1),
+    'end_date': datetime(2024, 1, 7),
+    'retries': 2,
+    'retry_delay': timedelta(hours=1)
+}
+
+with DAG(
+    'jinja_template',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=True
+) as dag:
+    
+    echo_ds_nodash = BashOperator(
+        task_id='echo_ds',
+        bash_command="echo {{ds_nodash}}"
+        # https://airflow.apache.org/docs/apache-airflow/stable/core-concepts/operators.html#concepts-jinja-templating
+    )
+
+echo_ds_nodash
