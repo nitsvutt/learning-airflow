6711bbb4736ca2fb141fb86b80837f6213ba8c36
nitsvutt <nitsvutt@gmail.com>
2024-06-17 21:53:07 +0700

    change common folder

--- airflow.sh
+++ airflow.sh
@@ -7,7 +7,7 @@ rm docker-compose.yaml
 wget -O docker-compose.yaml https://airflow.apache.org/docs/apache-airflow/2.8.2/docker-compose.yaml
 
 # create necessary directories
-mkdir ./dags ./plugins ./logs ../common/airflow_data
+mkdir ./dags ./plugins ./logs $COMMON/airflow_data
 
 # create an .env file to assign the current user ID
 echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
